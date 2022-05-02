10b6e8099c9a39da0ffcef9a6892eceb03243506
CleaverMahfuz <cleavermahfuz@gmail.com>
2022-05-02 08:29:27 +0500

    mergeTwoNumbers

--- MergeTwoSortedLists/index.js
+++ MergeTwoSortedLists/index.js
@@ -3,7 +3,7 @@ const mergeTwoSortedLists = (l1, l2) => {
     if (!l2) return l1;
     if (l1.val < l2.val) {
         l1.next = mergeTwoSortedLists(l1.next, l2);
-        return l1;
+        return l1;  
     } else {
         l2.next = mergeTwoSortedLists(l1, l2.next);
         return l2;
